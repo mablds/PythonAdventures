a3ff91f47133e07b6ce0daf9470cf14f13b28969
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-04-10 13:11:01 -0300

    updating

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-import pyautogui, time
+import pyautogui
+# 1919, 1079 = pyautogui.size()
 
 try:
+    print('testando')
+    # pyautogui.moveTo(900, 1060)
+    # pyautogui.click()
     pyautogui.hotkey('win', 'r')
     pyautogui.hotkey('delete')
     pyautogui.typewrite('chrome')
     pyautogui.hotkey('enter')
-    # pyautogui.hotkey('ctrl', 'l', interval=0.25)m.br
-    pyautogui.click(50, 350)
-    time.sleep(.1)
-    pyautogui.typewrite('https://app.pontonow.com.br/login')
+    pyautogui.PAUSE = 0.7
+    # pyautogui.hotkey('alt', 'tab')
+    # pyautogui.hotkey('ctrl', 'l')
+    # pyautogui.PAUSE = 0.5
+    pyautogui.moveTo(1000, 50)
+    pyautogui.typewrite('https://www.google.com.br/')
     pyautogui.hotkey('enter')
-    time.sleep(.10)
-    pyautogui.click(1000,550) # x , y
-    # pyautogui.typewrite('oh que doidera')
+    pyautogui.moveTo(1000, 500)
+    pyautogui.PAUSE = 2.0
+    pyautogui.click()
+
+
+    
+    # pyautogui.hotkey('ctrl', 'l', interval=0.25)m.br
+    # pyautogui.click(50, 350)
+    # pyautogui.PAUSE = 2.0
+    # pyautogui.typewrite('https://app.pontonow.com.br/login')
     # pyautogui.hotkey('enter')
+    # pyautogui.PAUSE = 6.5
+    # pyautogui.move(1000,550) # x , y
 
 except KeyboardInterrupt:
     print('\nDone.')
